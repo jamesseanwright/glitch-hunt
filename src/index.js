fad41cbf3cf8b1ba102118f428d9a26fed23ac50
James Wright <james.wright@net-a-porter.com>
2016-08-15 22:18:34 +0100

    More boilerplate

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 'use strict';
 
+var canvas = document.querySelector('.game');
+var context = canvas.getContext('2d');
+var text = document.querySelector('#text');
+
+scaleDown(canvas, context);
+
+context.drawImage(text, 20, 20);
+
 function gameLoop() {
 	requestAnimationFrame(gameLoop);
+}
+
+function scaleDown(canvas, context) {
+	canvas.width = canvas.clientWidth / 4;
+	canvas.height = canvas.clientHeight / 4;
+	context.imageSmoothingEnabled = false;
 }
 
 gameLoop();
