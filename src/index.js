12d079b77cb66a404d1c8e8900583e6f63270b6f
James Wright <james@jamesswright.co.uk>
2016-08-16 17:37:51 +0100

    Remove anti-aliasing from everything by having a separate, upscaled output context

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 'use strict';
 
-var canvas = document.querySelector('.game');
-var context = canvas.getContext('2d');
-var text = document.querySelector('#text');
+var renderingCanvas = document.querySelector('.game-renderer');
+var renderingContext = renderingCanvas.getContext('2d');
+var outputCanvas = document.querySelector('.game-output');
+var outputContext = outputCanvas.getContext('2d');
 
-scaleDown(canvas, context);
+var SCALE = 4;
 
-context.drawImage(text, 20, 20);
+initCanvases();
 
-function gameLoop() {
-	requestAnimationFrame(gameLoop);
+function initCanvases() {
+	renderingCanvas.width = window.innerWidth / SCALE;
+	renderingCanvas.height = window.innerHeight / SCALE;
+
+	outputCanvas.width = window.innerWidth;
+	outputCanvas.height = window.innerHeight;
+
+	outputContext.imageSmoothingEnabled = false;
+	outputContext.scale(SCALE, SCALE);
 }
 
-function scaleDown(canvas, context) {
-	canvas.width = canvas.clientWidth / 4;
-	canvas.height = canvas.clientHeight / 4;
-	context.imageSmoothingEnabled = false;
+function gameLoop(ts) {
+	renderingContext.clearRect(0, 0, 500, 36);	
+	renderingContext.font = '16px Courier New';
+	renderingContext.fillStyle = 'white';
+	renderingContext.fillText(ts, 20, 20);
+
+	outputContext.clearRect(0, 0, 500 * SCALE, 36 * SCALE);		
+	outputContext.drawImage(renderingCanvas, 0, 0);
+	requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
