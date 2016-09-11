ba2b13b84cfd1b069abd367e3b19d935b7576492
James Wright <james@jamesswright.co.uk>
2016-09-11 11:56:34 +0100

    Restricting keyboard movement to screen edges

--- src/systems/KeyboardMoveSystem.js
+++ src/systems/KeyboardMoveSystem.js
@@ -11,8 +11,8 @@
         for (var i = 0; i < G.keyboardMoveable.keys.length; i++) {
             key = G.keyboardMoveable.keys[i];
 
-            if (G.keyboard.isPressed(key.name)) {
-                entity.isMoving = true;                
+            if (G.keyboard.isPressed(key.name) && KeyboardMoveSystem.canMove(entity, key.direction)) {
+                entity.isMoving = true;
                 entity.direction = key.direction;
                 entity.x += entity.speed * entity.direction;
                 entity.onMove(entity.direction === G.keyboardMoveable.direction.LEFT);
@@ -26,6 +26,14 @@
             entity.isMoving = false;
         }
     });
+
+    KeyboardMoveSystem.canMove = function canMove(entity, direction) {
+        if (direction === G.keyboardMoveable.direction.LEFT) {
+            return entity.x >= 0;
+        }
+
+        return entity.x + entity.width <= G.constants.WORLD_WIDTH;
+    };
 
     G.KeyboardMoveSystem = KeyboardMoveSystem;
 }());
